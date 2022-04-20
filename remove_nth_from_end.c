aee9cf29996c591d865e97cc5cf98bdbcdf85d76
Mitrofanov Vladimir <nonameletters@gmail.com>
2022-04-20 12:08:41 +0300

    Create remove_nth_from_end.c
    Remove N-th element from end in single ended list using recursion and one shot path through

---
+++ remove_nth_from_end.c
@@ -0,0 +1,29 @@
+    void removeN(ListNode** node, int& fl, int n)
+    {
+        if ((*node) != NULL)
+        {
+            removeN(&((*node)->next), fl, n);
+        }
+        fl++;
+        
+        if (fl != n)
+            return;
+        
+        if ((*node)->next == NULL)
+        {
+            delete (*node);
+            (*node) = NULL;
+            return;
+        }
+        
+        ListNode* nxt = (*node)->next;
+        (*node)->val = (*node)->next->val;
+        (*node)->next = (*node)->next->next;
+        delete nxt;
+    }
+    
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int fl = -1;
+        removeN(&head, fl, n);
+        return head;
+    }
