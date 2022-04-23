6335dd49746cd2c0e10620fe3602a7e65b2908d5
Mitrofanov Vladimir <nonameletters@gmail.com>
2022-04-23 07:03:22 +0300

    Create reverse_linked_list.c

---
+++ reverse_linked_list.c
@@ -0,0 +1,12 @@
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        
+        while(head)
+        {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;            
+        }
+        return prev;
+    }
