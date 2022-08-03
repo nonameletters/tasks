851dbb834b48cf075500f38b8634d04f6ddec9d0
Mitrofanov Vladimir <nonameletters@gmail.com>
2022-08-03 11:33:30 +0300

    Create hamming_distance.cpp
    We know that XOR(A, B) resets to 0 all equivalent bits. After XOR equal bits will be zeroed. Then we count bits that equal 1.

---
+++ hamming_distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res = x ^ y;
+        int count = 0;
+        while(res != 0)
+        {
+            if (res & 1)
+                count++;
+            res >>= 1;
+        }
+        return count;
+    }
+};
