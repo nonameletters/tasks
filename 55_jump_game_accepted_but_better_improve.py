4159e327767fd7c0d59510f5754475867195d379
Mitrofanov Vladimir <nonameletters@gmail.com>
2024-05-24 08:54:36 +0300

    Update 55_jump_game_accepted_but_better_improve.py

--- 55_jump_game_accepted_but_better_improve.py
+++ 55_jump_game_accepted_but_better_improve.py
@@ -22,3 +22,26 @@ class Solution:
             if (j < 0):
                 return False
         return True
+
+# Second solution better but not well
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        i = 0
+        while i < len(nums):
+            if i == len(nums) - 1:
+                return True
+
+            if nums[i] != 0:
+                i += nums[i]
+                continue
+
+            j = i - 1
+            while (j >= 0):
+                if (nums[j] + j > i):
+                    i = j + nums[j]
+                    break
+                else:
+                    j -= 1
+            if (j <= 0):
+                return False
+        return True
