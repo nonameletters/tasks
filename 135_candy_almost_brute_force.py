0d70412ab05d3043e3633700ab9c2b446a5fa716
Mitrofanov Vladimir <nonameletters@gmail.com>
2025-08-06 09:38:32 +0300

    Update 135_candy_almost_brute_force.py

--- 135_candy_almost_brute_force.py
+++ 135_candy_almost_brute_force.py
@@ -6,60 +6,38 @@ class Solution:
         if len(ratings) == 1:
             return 1
 
-        i = 0
-        while i < len(ratings):
-            if i <= 0: # Most left
-                if ratings[i] > ratings[i + 1]:
-                    candies[i] = candies[i + 1] + 1
-                else:
-                    pass
-            elif i >= len(ratings) - 1: # Most right
-                if ratings[i] > ratings[i - 1]:
-                    candies[i] = candies[i - 1] + 1
-                else:
-                    pass
-            else: # Midle elemend we have left and right neighbours
-                if ratings[i] > ratings[i - 1]:
-                    if candies[i] <= candies[i - 1]:
+        def pass_through():
+            i = 0
+            while i < len(ratings):
+                if i <= 0: # Most left
+                    if ratings[i] > ratings[i + 1]:
+                        candies[i] = candies[i + 1] + 1
+                    else:
+                        pass
+                elif i >= len(ratings) - 1: # Most right
+                    if ratings[i] > ratings[i - 1]:
                         candies[i] = candies[i - 1] + 1
-                else:
-                    pass
+                    else:
+                        pass
+                else: # Midle elemend we have left and right neighbours
+                    if ratings[i] > ratings[i - 1]:
+                        if candies[i] <= candies[i - 1]:
+                            candies[i] = candies[i - 1] + 1
+                    else:
+                        pass
 
-                if ratings[i] > ratings[i + 1]:
-                    if candies[i] <= candies[i + 1]:
-                        candies[i] = candies[i] + 1
-                else:
-                    pass
-            i += 1
+                    if ratings[i] > ratings[i + 1]:
+                        if candies[i] <= candies[i + 1]:
+                            candies[i] = candies[i] + 1
+                    else:
+                        pass
+                i += 1
 
+        pass_through()
         ratings.reverse()
         candies.reverse()
-        
-        i = 0
-        while i < len(ratings):
-            if i <= 0: # Most left
-                if ratings[i] > ratings[i + 1]:
-                    candies[i] = candies[i + 1] + 1
-                else:
-                    pass
-            elif i >= len(ratings) - 1: # Most right
-                if ratings[i] > ratings[i - 1]:
-                    candies[i] = candies[i - 1] + 1
-                else:
-                    pass
-            else: # Midle elemend we have left and right neighbours
-                if ratings[i] > ratings[i - 1]:
-                    if candies[i] <= candies[i - 1]:
-                        candies[i] = candies[i - 1] + 1
-                else:
-                    pass
+        pass_through()
 
-                if ratings[i] > ratings[i + 1]:
-                    if candies[i] <= candies[i + 1]:
-                        candies[i] = candies[i] + 1
-                else:
-                    pass
-            i += 1
 
         print(f'{candies}')
         return sum(candies)
